137714d9f0d60bd7b7f900efbc408ec8c16eecc9
David-Chae <aksrud859@hanmail.net>
2021-11-14 00:21:52 +1100

    Update navigator.py
    Opens and closes search results automatically

--- src/navigator.py
+++ src/navigator.py
@@ -17,9 +17,22 @@ def main():
     startEmptySearchInSalesNav(driver)
     selectGeographyInSearch(driver, "Australia")
     selectTitlesInSearch(driver, titles)
-    selectCompaniesInSearch(driver, companies)
-    time.sleep(60)
-    driver.close()
+    #selectCompaniesInSearch(driver, companies)
+    page_num = getSearchResultPageNumber(driver)
+    print("You are now ready to move on to working with " + str(page_num) + " pages.")
+    #test(driver, "https://www.google.com.au")
+    results_num = getSearchResultsNumber(driver)
+    print("You are now ready to move on to working with " + str(results_num) + " results in current page.")
+
+    
+    curr = 1
+    while curr <= results_num:
+        openSearchResults(driver, curr)
+        curr+=1
+
+    
+    time.sleep(6)
+    driver.quit()
 
 
 def loginToLinkedInSalesNav(driver):    
@@ -50,6 +63,8 @@ def startEmptySearchInSalesNav(driver):
     try:
         wait = WebDriverWait(driver, 10)
         element = wait.until(EC.presence_of_element_located((By.ID,'global-typeahead-search-input')))
+    except:
+        driver.quit()
     finally:
         search_bar = driver.find_element_by_id('global-typeahead-search-input');
         search_bar.send_keys(Keys.RETURN)
@@ -60,7 +75,8 @@ def selectGeographyInSearch(driver, country):
     try:
         wait = WebDriverWait(driver, 10)
         element = wait.until(EC.presence_of_element_located((By.XPATH,'//form[@class="search-filter__form"]/ul/li[5]')))
-
+    except:
+        driver.quit()
     finally:
         geography = driver.find_element(By.XPATH, '//form[@class="search-filter__form"]/ul/li[5]')
         geography.click()
@@ -73,7 +89,8 @@ def selectGeographyInSearch(driver, country):
     try:
         wait = WebDriverWait(driver, 1)
         element = wait.until(EC.presence_of_element_located((By.XPATH,'//form[@class="search-filter__form"]/ul/li[5]//div[@class="ph4 pb4"]/ol/li[1]/button')))
-
+    except:
+        driver.quit()
     finally:
         geography_country_btn = driver.find_element(By.XPATH, '//form[@class="search-filter__form"]/ul/li[5]//div[@class="ph4 pb4"]/ol/li[1]/button')
         geography_country_btn.send_keys(Keys.RETURN)
@@ -84,7 +101,8 @@ def selectTitleInSearch(driver, title):
     try:
         wait = WebDriverWait(driver, 10)
         element = wait.until(EC.presence_of_element_located((By.XPATH,'//form[@class="search-filter__form"]/ul/li[12]')))
-
+    except:
+        driver.quit()
     finally:
         filter = driver.find_element(By.XPATH, '//form[@class="search-filter__form"]/ul/li[12]')
         filter.click()
@@ -99,7 +117,8 @@ def selectTitlesInSearch(driver, titles):
     try:
         wait = WebDriverWait(driver, 10)
         element = wait.until(EC.presence_of_element_located((By.XPATH,'//form[@class="search-filter__form"]/ul/li[12]')))
-
+    except:
+        driver.quit()
     finally:
         filter = driver.find_element(By.XPATH, '//form[@class="search-filter__form"]/ul/li[12]')
         filter.click()
@@ -115,7 +134,8 @@ def selectCompaniesInSearch(driver, companies):
     try:
         wait = WebDriverWait(driver, 10)
         element = wait.until(EC.presence_of_element_located((By.XPATH,'//form[@class="search-filter__form"]/ul/li[7]')))
-
+    except:
+        driver.quit()
     finally:
         filter = driver.find_element(By.XPATH, '//form[@class="search-filter__form"]/ul/li[7]')
         filter.click()
@@ -124,6 +144,54 @@ def selectCompaniesInSearch(driver, companies):
         for company in companies:
             filter_search_bar.send_keys(company)
             filter_search_bar.send_keys(Keys.RETURN)
+
+def getSearchResultPageNumber(driver):
+# I want to know the number of pages of search results
+    try:
+        wait = WebDriverWait(driver, 10)
+        element = wait.until(EC.presence_of_element_located((By.XPATH, '//section[@id="results"]/div/nav/ol[@class="search-results__pagination-list"]')))
+    except:
+        driver.quit()
+    finally:
+        html_list = driver.find_element(By.XPATH, '//section[@id="results"]/div/nav/ol[@class="search-results__pagination-list"]')
+        items = html_list.find_elements_by_tag_name("li")
+        page_num = len(items)
+        print("There are " + str(page_num) + " pages")
+
+    return page_num
+
+def getSearchResultsNumber(driver):
+    try:
+        wait = WebDriverWait(driver, 10)
+        element = wait.until(EC.presence_of_element_located((By.XPATH, '//section[@id="results"]/div/div/ol[@class="search-results__result-list"]')))
+    except:
+        driver.quit()
+    finally:
+        html_list = driver.find_elements(By.XPATH, '//section[@id="results"]/div/div/ol[@class="search-results__result-list"]/li')
+        results_num = len(html_list)
+        print("There are " + str(results_num) + " results in current page.")
+
+    return results_num
+
+def openSearchResults(driver, curr):
+    try:
+        wait = WebDriverWait(driver, 10)
+        element = wait.until(EC.presence_of_element_located((By.XPATH, '//section[@id="results"]/div/div/ol[@class="search-results__result-list"]/li['+ str(curr) + ']/div[2]/div/div/div/article/section[@class="result-lockup"]/div/div/dl/dt[@class="result-lockup__name"]/a')))
+    except:
+        driver.quit()
+    finally:
+        url = driver.find_element(By.XPATH, '//section[@id="results"]/div/div/ol[@class="search-results__result-list"]/li['+ str(curr) + ']/div[2]/div/div/div/article/section[@class="result-lockup"]/div/div/dl/dt[@class="result-lockup__name"]/a').get_attribute('href')
+        test(driver, url)
+    
+
+def test(driver, url):
+    driver.execute_script("window.open('');")
+    driver.switch_to.window(driver.window_handles[1])
+    driver.get(url);
+    time.sleep(3)
+    driver.close()
+    driver.switch_to.window(driver.window_handles[0])
+    
 
 
 main()
